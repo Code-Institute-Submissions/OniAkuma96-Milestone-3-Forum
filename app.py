56683c713e1e37dbd88797fcbed0acb8dff30d21
OniAkuma96 <harrison.tudball@hotmail.com>
2021-06-22 16:58:01 +0000

    add registration functionality, started new post functionality

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
+import datetime
 from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -21,6 +23,47 @@ mongo = PyMongo(app)
 def homepage():
     posts = mongo.db.forum_posts.find()
     return render_template("homepage.html", posts=posts)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username Already Exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/new_post", methods=["GET", "POST"])
+def new_post():
+    if request.method == "POST":
+        date_time = datetime.datetime.now()
+        post = {
+            "post_title": request.form.get("post_title"),
+            "post_description": request.form.get("post_description"),
+            "created_by": session["user"],
+            "creation_date": date_time.strftime("%x"),
+            "creation_time": date_time.strftime("%X")
+        }
+        mongo.db.forum_posts.insert_one(post)
+        flash("Post Successful")
+        return redirect(url_for("homepage"))
+
+    return render_template("new_post.html")
 
 
 if __name__ == "__main__":
